4b314787a62f9f2bc563c16b02fc7fd3e249aa3a
Florian Carrier <Akaizoku@users.noreply.github.com>
2019-10-21 11:45:26 +0100

    Fix Get-Path
    Fix an issue where newly created absolute paths were not updated in the list of paths

--- Public/Get-Path.ps1
+++ Public/Get-Path.ps1
@@ -1,4 +1,17 @@
 function Get-Path {
+  <#
+    .SYNOPSIS
+    Resolve relative path
+
+    .DESCRIPTION
+    Resolve relative path
+
+    .NOTES
+    File name:      Get-Path.ps1
+    Author:         Florian Carrier
+    Creation date:  27/11/2018
+    Last modified:  15/10/2019
+  #>
   [CmdletBinding ()]
   Param (
     [Parameter (
@@ -26,6 +39,10 @@ function Get-Path {
     [String]
     $Root = $PSScriptRoot
   )
+  Begin {
+    # Get global preference variables
+    Get-CallerPreference -Cmdlet $PSCmdlet -SessionState $ExecutionContext.SessionState
+  }
   Process {
     $Paths = Resolve-Array -Array $PathToResolve -Delimiter ","
     foreach ($Path in $Paths) {
@@ -34,14 +51,14 @@ function Get-Path {
       if ($Pathway -match "^.*\\") {
         $RelativePath = $Pathway -replace "^.*\\", ""
         $AbsolutePath = Join-Path -Path $Root -ChildPath $RelativePath
-        if (Test-Path -Path $AbsolutePath) {
-          $Hashtable.$Path = $AbsolutePath
-        } else {
-          Write-Log -Type "INFO" -Message "Creating directory: $AbsolutePath"
+        if (-Not (Test-Path -Path $AbsolutePath)) {
+          Write-Log -Type "INFO" -Object "Creating directory: $AbsolutePath"
           New-item -ItemType "Directory" -Path "$AbsolutePath" | Out-Null
         }
+        # Write-Log -Type "DEBUG" -Object $AbsolutePath
+        $Hashtable.$Path = $AbsolutePath
       } elseif (-Not (Test-Path -Path $Pathway)) {
-        Write-Log -Type "ERROR" -Message "Path not found: $Pathway"
+        Write-Log -Type "ERROR" -Object "Path not found: $Pathway"
       }
     }
     return $Hashtable
