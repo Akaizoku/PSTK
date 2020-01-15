0240bc46bb60460dca323c3314b6e194fae36ace
Florian Carrier <Akaizoku@users.noreply.github.com>
2020-01-15 17:49:30 +0000

    Format connection string

--- Public/Test-SQLConnection.ps1
+++ Public/Test-SQLConnection.ps1
@@ -1,71 +1,59 @@
-# ------------------------------------------------------------------------------
-# Database connection testing function
-# ------------------------------------------------------------------------------
 function Test-SQLConnection {
   <#
     .SYNOPSIS
-    Check a SQL Server database connection
+    Test SQL Server database connection
 
     .DESCRIPTION
-    Check that a SQL Server database connection is working
+    Check that a SQL Server database connection is working.
 
     .PARAMETER Server
-    [String] The server parameter corresponds to the database server to connect to
+    The server parameter corresponds to the database server to connect to.
 
     .PARAMETER Database
-    [String] The database parameter corresponds to the database to be tested
-
-    .PARAMETER Security
-    [DEPRECATED] The security parameter defines if the connection should be made us-
-    ing the SQL Server Integrated Security (Windows Active Directory) or the
-    default SQL authentication with username and password.
+    The database parameter corresponds to the database to be tested.
 
     .PARAMETER Username
-    [String] The username parameter corresponds to the username of the account
-    to use in case of SQL authentication.
+    The username parameter corresponds to the username of the account to use in case of SQL authentication.
 
     .PARAMETER Password
-    [String] The password parameter corresponds to the password of the account
-    to use in case of SQL authentication.
+    The password parameter corresponds to the password of the account to use in case of SQL authentication.
 
     .PARAMETER Credentials
-    [System.Management.Automation.PSCredential] The credentials parameter corresponds to the credentials of account
-    to use in case of SQL authentication.
+    The credentials parameter corresponds to the credentials of accoun to use in case of SQL authentication.
+
+    .PARAMETER TimeOut
+    The optional time-out parameter corresponds to the time in seconds before the connection is deemed unresponsive. The default value is 3 seconds.
+
+    .PARAMETER Security
+    [DEPRECATED] The security parameter defines if the connection should be made using the SQL Server Integrated Security (Windows Active Directory) or the default SQL authentication with username and password.
 
     .INPUTS
     None. You cannot pipe objects to Test-SQLConnection.
 
     .OUTPUTS
-    [Boolean] Test-SQLConnection returns a boolean depending on the result of the
-    connection attempt.
+    Boolean. Test-SQLConnection returns a boolean depending on the result of the connection attempt.
 
     .EXAMPLE
     Test-SQLConnection -Server "localhost" -Database "MSSQLServer"
 
-    In this example, Test-SQLConnection will try to connect to the MSSQLServer
-    database on the local server using the current Windows user.
+    In this example, Test-SQLConnection will try to connect to the database "MSSQLServer" on the server "localhost" using the current Windows user.
 
     .EXAMPLE
     Test-SQLConnection -Server "localhost" -Database "MSSQLServer" -Username "user" -Password "password"
 
-    In this example, Test-SQLConnection will try to connect to the MSSQLServer
-    database on the local server using the credentials of the user "user" with
-    the "password" password.
+    In this example, Test-SQLConnection will try to connect to the database "MSSQLServer" on the server "localhost" using the credentials of the user "user" with the password "password".
 
     .EXAMPLE
-    $SecurePassword = ConvertTo-SecureString -String "password" -AsPlainText -Force
-    $Credential = New-Object -TypeName "System.Management.Automation.PSCredential" -ArgumentList ("user", $SecurePassword)
+    $Credentials = New-Object -TypeName "System.Management.Automation.PSCredential" -ArgumentList ("user", (ConvertTo-SecureString -String "password" -AsPlainText -Force))
     Test-SQLConnection -Server "localhost" -Database "MSSQLServer" -Credentials $Credentials
 
-    In this example, Test-SQLConnection will try to connect to the MSSQLServer
-    database on the local server using the credentials of the user "user" with
-    the "password" password.
+    In this example, Test-SQLConnection will try to connect to the database "MSSQLServer" on the server "localhost" using the credentials of the user "user" with the password "password".
 
     .NOTES
     File name:      Test-SQLConnection.ps1
     Author:         Florian Carrier
     Creation date:  15/10/2018
-    Last modified:  02/12/2019
+    Last modified:  15/01/2020
     Dependencies:   Test-SQLConnection requires the SQLServer module
 
     .LINK
@@ -118,6 +106,14 @@ function Test-SQLConnection {
     [System.Management.Automation.PSCredential]
     $Credentials,
     [Parameter (
+      Position    = 7,
+      Mandatory   = $false,
+      HelpMessage = "Connection timeout (in seconds)"
+    )]
+    [ValidateNotNullOrEmpty ()]
+    [Int]
+    $TimeOut = 3,
+    [Parameter (
       HelpMessage = "[DEPRECATED] Use of specific credentials instead of integrated security"
     )]
     [Switch]
@@ -129,21 +125,19 @@ function Test-SQLConnection {
   }
   Process {
     # Define connection string
-    $ConnectionString = "Server=$Server; Database=$Database; Connect Timeout=3;"
+    $ConnectionString = "Server=$Server;Database=$Database;Connect Timeout=$TimeOut;"
     # Check authentication mode
     if ($PSBoundParameters.ContainsKey("Credentials")) {
       # If "secured" credentials are provided
-      $FullConnectionString = $ConnectionString + "Integrated Security=False; User ID=$($Credentials.Username); Password=$($Credentials.GetNetworkCredential().Password);"
-      $Obfuscate            = $Credentials.GetNetworkCredential().Password
+      $FullConnectionString = $ConnectionString + "Integrated Security=False;User ID=$($Credentials.Username);Password=$($Credentials.GetNetworkCredential().Password);"
+      $SensitiveData        = $Credentials.GetNetworkCredential().Password
     } elseif ($PSBoundParameters.ContainsKey("Username") -And $PSBoundParameters.ContainsKey("Password")) {
       # If plain text credentials are provided
       if ($Username) {
-        $FullConnectionString = $ConnectionString + "Integrated Security=False; User ID=$Username; Password=$Password;"
-        $Obfuscate            = $Password
+        $FullConnectionString = $ConnectionString + "Integrated Security=False;User ID=$Username;Password=$Password;"
+        $SensitiveData        = $Password
       } else {
-        Write-Log -Type "ERROR" -Message "Please provide a valid username"
-        Write-Log -Type "DEBUG" -Message "$Username"
-        Stop-Script 1
+        Write-Log -Type "ERROR" -Message "Invalid username ""$Username""" -ExitCode 1
       }
     } else {
       # Else default to integrated security (Windows authentication)
@@ -151,7 +145,7 @@ function Test-SQLConnection {
       $FullConnectionString = $ConnectionString + "Integrated Security=True;"
     }
     # Create connection object
-    Write-Log -Type "DEBUG" -Object $FullConnectionString -Obfuscate $Obfuscate
+    Write-Log -Type "DEBUG" -Object $FullConnectionString -Obfuscate $SensitiveData
     $Connection = New-Object -TypeName "System.Data.SqlClient.SqlConnection" -ArgumentList $FullConnectionString
     # Try to open the connection
     try {
