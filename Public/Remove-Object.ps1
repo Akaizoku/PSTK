6ea5b3eb582808b2e419e336d8d6d538f7df4b4f
Florian <Akaizoku@users.noreply.github.com>
2021-07-07 18:05:22 +0100

    Update Remove-Object.ps1
    Clean-up

--- Public/Remove-Object.ps1
+++ Public/Remove-Object.ps1
@@ -1,17 +1,30 @@
-# ------------------------------------------------------------------------------
-# Generic Remove-Item with checks
-# ------------------------------------------------------------------------------
 function Remove-Object {
   <#
     .SYNOPSIS
-    Remove objects
+    Remove object(s)
 
     .DESCRIPTION
-    Remove list of objects matching specifications
+    Remove list of objects matching specifications (wrapper for Remove-Item with checks)
 
-    .NOTES
+    .PARAMETER Path
+    The path parameter corresponds to the location of the objects to remove.
+
+    .PARAMETER Type
+    The type parameter corresponds to the type of objects to remove.
+
+    .PARAMETER Filter
+    The filter parameter corresponds to the filter to apply to the name of objects to remove.
+
+    .PARAMETER Exclude
+    The exclude parameter corresponds to the filter to apply to the name of objects *not* to remove.
     /!\ Exclude is currently not supported in Windows PowerShell
     See https://github.com/PowerShell/PowerShell/issues/6865
+
+    .NOTES
+    File name:      Remove-Object.ps1
+    Author:         Florian Carrier
+    Creation date:  2019-06-14
+    Last modified:  2021-07-06
   #>
   [CmdletBinding ()]
   Param (
@@ -21,7 +34,7 @@ function Remove-Object {
       HelpMessage = "Path to the items"
     )]
     [ValidateNotNullOrEmpty ()]
-    [String]
+    [System.String]
     $Path,
     [Parameter (
       Position    = 2,
@@ -33,32 +46,36 @@ function Remove-Object {
       "File",
       "Folder"
     )]
-    [String]
+    [System.String]
     $Type = "All",
     [Parameter (
       Position    = 3,
       Mandatory   = $false,
       HelpMessage = "Filter to apply"
     )]
-    [String]
+    [ValidateNotNullOrEmpty ()]
+    [System.String]
     $Filter = "*",
     [Parameter (
       Position    = 4,
       Mandatory   = $false,
       HelpMessage = "Pattern to exclude"
     )]
-    [String]
+    [ValidateNotNullOrEmpty ()]
+    [System.String[]]
     $Exclude = $null
   )
   Begin {
+    # Get global preference vrariables
+    Get-CallerPreference -Cmdlet $PSCmdlet -SessionState $ExecutionContext.SessionState
+    # Check path
     $Path = Resolve-Path -Path $Path
-    if (-Not (Test-Path -Path $Path)) {
-      Write-Log -Type "ERROR" -Message "$Path does not exists."
-      Stop-Script 1
+    if (Test-Object -Path $Path -NotFound) {
+      Write-Log -Type "ERROR" -Message "Path not found $Path" -ErrorCode 1
     }
   }
   Process {
-    $Objects = New-Object -TypeName System.Collections.ArrayList
+    $Objects = New-Object -TypeName "System.Collections.ArrayList"
     # Check PowerShell version to prevent issue
     $PSVersion = $PSVersionTable.PSVersion | Select-Object -ExpandProperty "Major"
     if ($PSVersion -lt 6) {
@@ -67,10 +84,16 @@ function Remove-Object {
       $Objects = Get-Object -Path $Path -Type $Type -Filter $Filter -Exclude $Exclude
     }
     # If objects are found
-    if ($Objects. Count -gt 0) {
+    if ($Objects.Count -ge 1) {
       foreach ($Object in $Objects) {
-        Write-Log -Type "DEBUG" -Object $Object
-        Remove-Item -Path $Path -Recurse -Force
+        if ($null -ne $Object) {
+          Write-Log -Type "DEBUG" -Object $Object
+          try {
+            Remove-Item -Path $Object.FullName -Recurse -Force
+          } catch {
+            Write-Log -Type "ERROR" -Message $Error[0].Exception.Message
+          }
+        }
       }
     }
   }
